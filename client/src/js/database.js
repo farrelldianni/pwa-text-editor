3442889a3dcc3c8143f40c800705151cc2bf1d50
frl <103608522+farrelldianni@users.noreply.github.com>
2022-10-13 15:42:51 -0400

    get/put routes done-ish

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, value) => {
+  console.log('putting update to jateDB');
+  const jateDB = await openDB('jate', '1');
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const objStore = tx.objectStore('jate');
+  const req = req.objStore({id: id, value:value})
+  const res = await req;
+  console.log('saved to jateDB');
+
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (value) => {
+  console.log('getting from jateDB');
+  const jateDB = await openDB('jate', '1');
+  //specifying new transaction and permissions 
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const objStore = tx.objectStore('jate');
+  //.getAll to pull all DB data 
+  const req = objStore.getAll()
+  const res = await req;
+  console.log('data saved')
+};
 
 initdb();
